d3b9ee73de97366351000457731c25789189f6cf
unknown <shahid31346@gmail.com>
2024-06-25 11:10:17 +0500

    abc

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,11 @@
 // import 'package:counter_app/firebase_options.dart';
 // import 'package:counter_app/utils/const.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:suvastufood/firebase_options.dart';
-import 'package:suvastufood/test/test.dart';
-import 'package:suvastufood/user_side/screen/auth_screens/get_started_screen.dart';
-import 'package:suvastufood/user_side/screen/auth_screens/login_screen.dart';
-import 'package:suvastufood/user_side/screen/onboarding_screen/onboarding_screen.dart';
-import 'package:suvastufood/user_side/screen/restaurant_flow/food_details.dart';
+import 'package:suvastufood/user_side/main_tabs.dart';
 import 'package:suvastufood/utils/const.dart';
 import 'package:suvastufood/utils/translation.dart';
 
@@ -20,6 +14,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  
   await GetStorage.init();
   runApp(MyApp());
 }
@@ -42,7 +37,7 @@ class MyApp extends StatelessWidget {
           Locale(appData.read('language') ?? 'en'), // Default locale is English
       fallbackLocale: Locale('en_US'), // Fallback locale is English
       debugShowCheckedModeBanner: false,
-      home: FoodDetailsScreen(),
+      home: MainTabs(),
     );
   }
 }
